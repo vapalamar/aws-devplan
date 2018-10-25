fc3fa13c29743b6b10589e24b207823c238f3c66
Vyacheslav Palamar <vyacheslav.palamar@da-14.com>
2018-10-25 17:35:23 +0300

    Basic Hapi server works

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const Hapi = require("hapi");
+
+const server = Hapi.server({
+  port: 3000,
+  host: "0.0.0.0"
+});
+
+const init = async () => {
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
